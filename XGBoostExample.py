f46b448e1574aae3d35d17927cc532b0f7d9e3ba
ChamodDamitha <chamod.14@cse.mrt.ac.lk>
2018-04-02 14:48:45 +0530

    Add XGBoost tuning parameters

--- XGBoostExample.py
+++ XGBoostExample.py
@@ -22,9 +22,18 @@ train_X, test_X, train_y, test_y = train_test_split(X.as_matrix(), y.as_matrix()
 
 from xgboost import XGBRegressor
 
-my_model = XGBRegressor()
+#my_model = XGBRegressor()
 # Add silent=True to avoid printing out updates with each cycle
-my_model.fit(train_X, train_y, verbose=False)
+#my_model.fit(train_X, train_y, verbose=False)
+
+#model tuning
+#my_model = XGBRegressor(n_estimators=1000)
+             
+#add learning rate
+my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
+my_model.fit(train_X, train_y, early_stopping_rounds=5, 
+             eval_set=[(test_X, test_y)], verbose=False)
+
 
 # make predictions
 predictions = my_model.predict(test_X)
